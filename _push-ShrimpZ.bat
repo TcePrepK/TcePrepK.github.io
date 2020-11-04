43dc467a5e47cdcdb5cb0c7aed2dad0d5217f73a
TcePrepK <56453014+TcePrepK@users.noreply.github.com>
2020-11-04 20:33:29 +0300

    _push-ShrimpModBrowser

--- _push-ShrimpZ.bat
+++
@@ -1,9 +0,0 @@
-robocopy "../shapez.io/src/" "./" "index.html"
-
-robocopy "../shapez.io/build/" "./shapez/ShrimpZ-WirelessDisplay" /S
-
-git add -A
-
-git commit -m "_push-ShrimpZ"
-
-git push --force

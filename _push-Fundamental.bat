0200b8b7798404ea1b9a6adda33a35bbb2e51c79
TcePrepK <a.s.arda.simsek@gmail.com>
2024-08-01 16:53:56 +0300

    _default-push

--- _push-Fundamental.bat
+++
@@ -1,8 +0,0 @@
-cd Fundamental
-for /f %%i in ('git rev-parse HEAD') do set commit_id=%%i
-set short_commit_id=%commit_id:~0,7%
-
-cd ../
-git add .
-git commit -m "Fundamental %short_commit_id%"
-git push

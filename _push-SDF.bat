76cc0e0c268234ee3ca30b1742bacf39a62641be
TcePrepK <a.s.arda.simsek@gmail.com>
2024-08-06 14:34:03 +0300

    SDF_Designer 8f54b90

--- _push-SDF.bat
+++ _push-SDF.bat
@@ -2,7 +2,7 @@ cd ../SDF_Designer
 for /f %%i in ('git rev-parse HEAD') do set commit_id=%%i
 set short_commit_id=%commit_id:~0,7%
 
-robocopy "dist/" "../TcePrepKgithubio/SDF_Designer" /S 
+robocopy "build/" "../TcePrepKgithubio/SDF_Designer" /S 
 cd ../TcePrepKgithubio
 git add .
 git commit -m "SDF_Designer %short_commit_id%"

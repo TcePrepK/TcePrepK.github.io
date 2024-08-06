3b8f81d1eefd1d0946d09a74101512b90063b192
TcePrepK <a.s.arda.simsek@gmail.com>
2024-08-07 02:37:50 +0300

    SDF_Designer 39f2cf4

--- _push-SDF.bat
+++ _push-SDF.bat
@@ -1,7 +1,6 @@
-cd ../SDF_Designer
+cd ../SDF_Studio
 for /f %%i in ('git rev-parse HEAD') do set commit_id=%%i
 set short_commit_id=%commit_id:~0,7%
-
 
 call npm run build
 robocopy "dist/" "../TcePrepKgithubio/SDF_Designer" /S 

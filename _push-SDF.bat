cf3c8810f62304a9c449d619b4e4178b543be438
TcePrepK <a.s.arda.simsek@gmail.com>
2024-08-07 02:36:48 +0300

    SDF_Designer 76cc0e0

--- _push-SDF.bat
+++ _push-SDF.bat
@@ -2,8 +2,11 @@ cd ../SDF_Designer
 for /f %%i in ('git rev-parse HEAD') do set commit_id=%%i
 set short_commit_id=%commit_id:~0,7%
 
-robocopy "build/" "../TcePrepKgithubio/SDF_Designer" /S 
+
+call npm run build
+robocopy "dist/" "../TcePrepKgithubio/SDF_Designer" /S 
 cd ../TcePrepKgithubio
+
 git add .
 git commit -m "SDF_Designer %short_commit_id%"
 git push

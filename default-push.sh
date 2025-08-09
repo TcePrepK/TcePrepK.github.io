fce5f98daae3259bec56c6d21992c57cd921fabd
TcePrepK <a.s.arda.simsek@gmail.com>
2025-08-09 21:05:35 +0300

    Default push

---
+++ default-push.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import subprocess
+import shutil
+
+def main():
+    # Commit and push changes
+    subprocess.run(['git', 'add', '.'], check=True)
+    commit_message = f"Default push"
+    subprocess.run(['git', 'commit', '-m', commit_message], check=True)
+    subprocess.run(['git', 'push'], check=True)
+
+if __name__ == "__main__":
+    main()
